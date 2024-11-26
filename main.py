6c57b14ddf0afa6d21fc7422437fc844f4fee959
eleangiemendoza <eleangiemendoza@gmail.com>
2024-11-26 20:18:52 +0000

    updateeee

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import streamlit as st
 
 st.title("Valepro")
-st.caption("Soy una estudiante pro de matematicas :b")
-st.caption("VIVA LA YUCA Y LA JAGUA")
+st.header("Huoli")
+st.subheader("BUENAAAS")
+
+
+st.markdown("""
+Viva la ***yuca***, la papa, el queso y los *buñuelos*.
+
+**VIVA LA NAVIDA Y LA PROGRAMACIÓN**
+
+1. Yo
+2. Tu
+
++ yuca
++ papa
+
+:rainbow[VIVA EL PAN]
+""")
+
+st.latex("a^2 + b^2 = c^2")
+
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZmO1Tb0_xNRPGzCmsFixIy6JGVdUOdRNtug&s")
+
+st.video("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2yfkEAt2ew0&psig=AOvVaw1GnguVUzbm24bHrrnA8Vao&ust=1732738560592000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCPjlwuzo-okDFQAAAAAdAAAAABAE")
